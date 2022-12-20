76e4070c1bfc4759dd5a84eb9d6cbe10fb85c071
Taek-min <jtm11245@gmail.com>
2022-12-20 13:29:43 +0900

    1일차 문제풀이

---
+++ Week01/Day1/jtm_1297.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int diag, heightR, widthR, height, width;
+    cin >> diag >> heightR >> widthR;
+    cout<< (int)(diag/sqrt(pow(heightR, 2) + pow(widthR, 2))*heightR) << " " <<  (int)(diag/sqrt(pow(heightR, 2) + pow(widthR, 2))* widthR);
+}
