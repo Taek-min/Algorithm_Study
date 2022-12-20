f8b2a7011c0579968d4e3df2fe89d076632d36ac
ckdals6932 <ckdals6932@naver.com>
2022-12-20 17:38:51 +0900

    1일차 창민

---
+++ Week01/Day1/kcm_1297.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace week01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            double[] TV = new double[str.Length];
+            double OneRatio;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                TV[i] = double.Parse(str[i]);
+            }
+
+            OneRatio = Math.Sqrt(Math.Pow(TV[0], 2) / (Math.Pow(TV[1], 2) + Math.Pow(TV[2], 2)));
+
+            for (int j = 1; j < str.Length; j++)
+            {
+                TV[j] = TV[j] * OneRatio;
+            }
+            Console.WriteLine(Math.Truncate(TV[1]) + " " + Math.Truncate(TV[2]));
+        }
+    }
+}
