3305fd00291dfbd1a3f7a2cc973ea54ef78e367e
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2022-12-20 18:06:19 +0900

    Day1

---
+++ Week01/Day1/shm_1297.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _221220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            String[] num = input.Split(' ');
+            double X = Math.Sqrt(Math.Pow(int.Parse(num[0]), 2) / (Math.Pow(int.Parse(num[1]), 2) + Math.Pow(int.Parse(num[2]), 2)));
+            double H = Math.Truncate(Double.Parse(num[1]) * X);
+            double W = Math.Truncate(Double.Parse(num[2]) * X);
+            Console.WriteLine("{0} {1}", H, W);
+        }
+    }
+}
