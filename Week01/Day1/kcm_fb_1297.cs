c24e81a7ca43fae593c1c7104dafda7202205fe7
ckdals6932 <ckdals6932@naver.com>
2022-12-22 21:01:52 +0900

    1일차 피드백 반영

---
+++ Week01/Day1/kcm_fb_1297.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace week01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            double[] TV = new double[str.Length];
+            double OneRatio;
+
+            OneRatio = Math.Sqrt(Math.Pow(double.Parse(str[0]), 2) / (Math.Pow(double.Parse(str[1]), 2) + Math.Pow(double.Parse(str[2]), 2)));
+            Console.WriteLine(Math.Truncate(double.Parse(str[1]) * OneRatio) + " " + Math.Truncate(double.Parse(str[2]) * OneRatio));
+        }
+    }
+}
