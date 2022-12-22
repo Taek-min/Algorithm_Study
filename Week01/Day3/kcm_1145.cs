c53701d037bdacf482da0f3db4695387088e5770
ckdals6932 <ckdals6932@naver.com>
2022-12-22 20:50:37 +0900

    3일차 피드백 및 문제

---
+++ Week01/Day3/kcm_1145.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace kcm_1145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            int[] nums = new int[str.Length];
+            int count = 0;
+            int min;
+            for (int i = 0; i < str.Length; i++)
+            {
+                nums[i] = int.Parse(str[i]);
+            }
+
+            for (int num = 4; ; num++)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if(num % nums[i] == 0)
+                    {
+                        count++;
+                    }
+                }
+
+                if(count >= 3)
+                {
+                    min = num;
+                    break;
+                }
+
+                count = 0;
+            }
+            Console.Write(min);
+        }
+    }
+}
