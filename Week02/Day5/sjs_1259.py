c7e1013fb3caab15fb63869f252061df6300a40b
shinjunsu <80402795+shinjunsu@users.noreply.github.com>
2022-12-26 18:31:43 +0900

    Update sjs_1259.py

---
+++ Week02/Day5/sjs_1259.py
@@ -0,0 +1,17 @@
+#팰린드롬수_221226
+
+while(True):
+    word = str(input())
+    yorn = True
+    word = list(word)
+    for i in range(0,len(word)//2): 
+
+        if word[i] == word[len(word)-1 - i] :
+            continue
+        else :
+            yorn = False
+    yorn  = True
+
+    if yorn : print('no')
+    else : print('yes')       
+
