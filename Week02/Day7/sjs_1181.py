cbd991f64da5704116ebcd2b766308515db4b273
shinjunsu <80402795+shinjunsu@users.noreply.github.com>
2022-12-28 23:23:04 +0900

    Update sjs_1181.py

---
+++ Week02/Day7/sjs_1181.py
@@ -0,0 +1,16 @@
+N  = int(input())
+
+word = []
+for i in range(N):
+    word.append(input())
+
+setWord = list(set(word))
+
+sortedWord = []
+for i in setWord:
+    sortedWord.append((len(i),i))
+
+result = sorted(sortedWord)
+
+for lenWord, word in result:
+    print(word)
