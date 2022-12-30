0a783c5436ab3580ba60f9421ec1ffd5335400a5
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2022-12-30 17:53:41 +0900

    Day9

---
+++ Week02/Day9/shm_1789.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _221230
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint num = uint.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Truncate((-1.0 + Math.Sqrt(1.0 + 8.0 * num)) / 2.0));
+        }
+    }
+}
