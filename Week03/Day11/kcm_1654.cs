4632b1b4cc99741f7ec47d540d1b1a3580af4b17
ckdals6932 <ckdals6932@naver.com>
2023-01-05 19:30:07 +0900

    11일차 문제

---
+++ Week03/Day11/kcm_1654.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace kcm_1654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(); // 0값은 보유 랜선 개수, 1값이 필요 랜선 개수
+            long[] nums = new long[long.Parse(str[0])];
+
+            for(int j = 0; j < nums.Length; j++)
+            {
+                nums[j] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(nums);
+            long Start = 0;
+            long End = nums[nums.Length - 1] + 1;
+            long max = 0;
+
+            while (Start  + 1 < End)
+            {
+                long mid = (Start + End) / 2;
+
+                if (Check(mid, max, nums, str))
+                {
+                    Start = mid;
+
+                    if(mid > max)
+                    {
+                        max = mid;
+                    }
+                    
+                }
+                else
+                {
+                    End = mid;
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+
+        static bool Check(long mid, long max, long[] nums, string [] str)
+        {
+            long LANline = 0; 
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                LANline += nums[i] / mid;
+            }
+
+            if(LANline < int.Parse(str[1]))
+            {
+                return false;
+            }
+            else
+            { 
+                return true;
+            }
+            
+        }  
+    }
+}
