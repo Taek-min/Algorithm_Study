d0be3d6f104989cee13dc95f17eb501ebd9b9902
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2023-01-10 18:17:19 +0900

    Day13

---
+++ Week04/Day13/shm_1436.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int seq = 0;
+            while (num > 0)
+            {
+                seq++;
+                string str = seq.ToString();
+                if (str.Contains("666")) num--;
+            }
+            Console.WriteLine(seq);
+        }
+    }
+}
