b670b8ef3f08348c8602d6f378de147c7038236c
ckdals6932 <ckdals6932@naver.com>
2023-01-11 13:26:38 +0900

    13일차 문제풀이

---
+++ Week04/Day13/kcm_1436.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace kcm_1436
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int series = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 666; ; i++)
+            {
+                if (i.ToString().Contains("666"))
+                {
+                    count++;
+
+                    if (count == series)
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
