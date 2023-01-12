fcaa5bc9df4cb53224039ff86c3c10ea1278d03c
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2023-01-13 00:01:15 +0900

    Day15

---
+++ Week04/Day15/shm_1874.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            StringBuilder sb = new StringBuilder();
+            string[] seq = new string[loop];
+            int index = 0;
+            int cnt = 1;
+
+            for(int i = 0; i < loop; i++) seq[i] = Console.ReadLine();
+
+            stack.Push(cnt);
+            sb.AppendLine("+");
+            while (true)
+            {
+                if (index == loop) break;
+
+                if (stack.Count > 0)
+                {
+                    if(stack.Peek() == int.Parse(seq[index]))
+                    {
+                        sb.AppendLine("-");
+                        stack.Pop();
+                        index++;
+                        continue;
+                    }
+                    else
+                    {
+                        if (stack.Contains(int.Parse(seq[index])))
+                        {
+                            sb.Clear();
+                            sb.AppendLine("NO");
+                            break;
+                        }
+                    }
+                }
+                sb.AppendLine("+");
+                stack.Push(++cnt);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
