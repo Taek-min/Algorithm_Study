a8e5fbd273428be9a4f1a950767b7a2e4dee4989
ckdals6932 <ckdals6932@naver.com>
2023-01-13 18:46:38 +0900

    14,15 문제

---
+++ Week04/Day14/kcm_11866.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace kcm_11866
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split();
+            bool[] circle = new bool[int.Parse(num[0])];
+            StringBuilder strBuilder = new StringBuilder();
+            int count = 0;
+
+            while (count < int.Parse(num[1]) * int.Parse(num[0]))
+            {
+                for (int i = 0; i < circle.Length; i++)
+                {
+                    if (!circle[i])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    if(count % int.Parse(num[1]) == 0)
+                    {
+                        if(count == int.Parse(num[1]) * int.Parse(num[0]))
+                        { 
+                            strBuilder.Append(i + 1);
+                            break;
+                        }
+                        circle[i] = true;
+                        strBuilder.Append(i + 1);
+                        strBuilder.Append(", ");
+                    }
+                }
+            }
+
+            Console.WriteLine("<" + strBuilder + ">");
+        }
+    }
+}
---
+++ Week04/Day15/kcm_1874.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kcm_1874
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int[] nums = new int[int.Parse(num)];
+            Stack<int> stack = new Stack<int>();
+            List<int> checknum = new List<int>();
+            StringBuilder strBuilder = new StringBuilder();
+            int index = 0;
+            int count = 2;
+            bool check = true;
+            
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            stack.Push(1);
+            strBuilder.Append("+\n");
+
+            while (index < int.Parse(num))
+            {
+                if (stack.Count > 0)
+                {
+                    if (stack.Peek() - nums[index] > 0)
+                    {
+                        strBuilder.Append("-\n");
+                        check = false;
+                        break;
+                    }
+                    else if (stack.Peek() - nums[index] < 0)
+                    {
+                        strBuilder.Append("+\n");
+                        stack.Push(count);
+                        count++;
+                    }
+                    else
+                    {
+                        index++;
+                        strBuilder.Append("-\n");
+                        checknum.Add(stack.Peek());
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    strBuilder.Append("+\n");
+                    stack.Push(count);
+                    count++;
+                }
+            }
+
+            if (check)
+            {
+                Console.WriteLine(strBuilder);
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
