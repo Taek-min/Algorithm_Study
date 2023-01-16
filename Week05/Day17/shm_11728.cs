305b20c3d40e7a8ed4161ff1991e85cc427ec46b
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2023-01-17 02:30:58 +0900

    Day17

---
+++ Week05/Day17/shm_11728.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230116_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StringBuilder sb = new StringBuilder();
+            List<int> list = new List<int>();
+
+            sw.AutoFlush = true;
+            string[] input = sr.ReadLine().Split();
+            int one = int.Parse(input[0]);
+            int two = int.Parse(input[1]);
+
+            string[] first = sr.ReadLine().Split();
+            for (int i = 0; i < one; i++) list.Add(int.Parse(first[i]));
+
+            string[] second = sr.ReadLine().Split();
+            for (int i = one; i < one + two; i++) list.Add(int.Parse(second[i - one]));
+
+            list.Sort();
+            foreach (int N in list)
+            {
+                sb.Append(N + " ");
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
