9db6e275823f81ae02ae50588a4bf72c4582582f
ckdals6932 <ckdals6932@naver.com>
2023-01-19 13:45:51 +0900

    20일차 문제 풀이

---
+++ Week05/Day20/kcm_1929.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kcm_1929
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            bool[] nums = new bool[int.Parse(str[1]) + 1];
+            StringBuilder strBuilder = new StringBuilder();
+
+            if (int.Parse(str[1]) == 1)
+            {
+                return;
+            }
+
+            for (int i = 2; i <= int.Parse(str[1]); i++)
+            {
+                nums[i] = true;
+            }
+
+            for (int j = 2; j * j <= int.Parse(str[1]); j++)
+            {
+                if (nums[j])
+                {
+                    for (int k = j * j; k <= int.Parse(str[1]); k += j)
+                    {
+                        nums[k] = false;
+                    }
+                }
+            }
+
+            for (int l = int.Parse(str[0]); l <= int.Parse(str[1]); l++)
+            {
+                if (nums[l])
+                {
+                    strBuilder.Append(l + "\n");
+                }
+            }
+
+            Console.WriteLine(strBuilder.ToString());
+        }
+    }
+}
