059f8ec24c4d2c1eaafcedacbe5a6dac40f0a157
Taek-min <jtm11245@gmail.com>
2023-01-25 16:29:18 +0900

    문제풀이

---
+++ Week06/Day22/jtm_2751.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int N, num;
+    int* arrNum = new int[N];
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> num;
+        arrNum[i] = num;
+    }
+    sort(arrNum, arrNum+N);
+    for(int i = 0; i < N; i++){
+        cout << arrNum[i] << "\n";
+    }
+    delete[] arrNum;
+}
