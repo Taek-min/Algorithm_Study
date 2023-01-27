ddebd08bf7ff07336eeec2dbe1ec827fb986ac4a
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2023-01-27 10:42:57 +0900

    Update shm_14405.cs

--- Week06/Day24/shm_14405.cs
+++ Week06/Day24/shm_14405.cs
@@ -12,10 +12,10 @@ namespace _20230127
             StringBuilder sb = new StringBuilder();
             sw.AutoFlush = true;
             string input = sr.ReadLine();
-
-            if (input.Contains("pi")) input = input.Replace("pi", " ");
-            if (input.Contains("ka")) input = input.Replace("ka", " ");
-            if (input.Contains("chu")) input = input.Replace("chu", " ");
+      
+            input = input.Replace("pi", " ");
+            input = input.Replace("ka", " ");
+            input = input.Replace("chu", " ");
  
             if (input.Trim() == "") sb.Append("YES");
             else sb.Append("NO");
