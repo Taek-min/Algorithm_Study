1c1f9c545a6ecc20a178d45dc79795ec58883d64
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2023-02-02 19:02:37 +0900

    Day28

---
+++ Week07/Day28/shm_2851.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _20230202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            StringBuilder sb = new StringBuilder();
+            sw.AutoFlush = true;
+            int sum = 0;
+            int total = 100;
+            bool chk = false;
+            for(int i = 0; i < 10; i++)
+            {
+                int mushroom = int.Parse(sr.ReadLine());
+                if (chk) continue;
+                sum += mushroom;
+
+                if (sum == total)
+                {
+                    sb.Append(sum);
+                    chk = true;
+                }
+                else if (sum > total)
+                {
+                    if (Math.Abs(total - sum) > Math.Abs(total - (sum - mushroom))) sb.Append(sum - mushroom);
+                    else sb.Append(sum);
+                    chk = true;
+                }
+                else if (sum < total) if (i == 9) sb.Append(sum);
+            }
+            sw.WriteLine(sb);
+        }
+    }
+}
