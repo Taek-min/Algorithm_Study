69321fd4ab273c10e76711a4e5160b2ecbcb2e73
ckdals6932 <ckdals6932@naver.com>
2023-02-13 18:23:36 +0900

    35일 문제

---
+++ Week09/Day35/kcm_11050.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace kcm_11050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split();
+
+            Console.WriteLine(Factorial(int.Parse(num[0])) / (Factorial(int.Parse(num[0]) - int.Parse(num[1])) * Factorial(int.Parse(num[1]))));
+        }
+
+        static int Factorial(int num)
+        {
+            if(num <= 1)
+            {
+                return 1;
+            }
+
+            return num * Factorial(num -1);
+        }
+    }
+}
