210da129c34f1aff40849890bd008f9ff0f119a3
ckdals6932 <ckdals6932@naver.com>
2023-02-14 10:12:56 +0900

    36일차 문제

--- README.md
+++ README.md
@@ -52,6 +52,7 @@
 [<sub>![Bronze1]</sub> 피보나치 수 2](https://www.acmicpc.net/problem/2748) |
 [<sub>![Bronze1]</sub> 이항 계수 1](https://www.acmicpc.net/problem/11050) |
 [<sub>![Silver4]</sub> 숫자 카드 1](https://www.acmicpc.net/problem/10816) |
+[<sub>![Silver3]</sub> 프린터 큐](https://www.acmicpc.net/problem/1966) |
 
 
 
---
+++ Week09/Day36/kcm_10816.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace kcm_10816
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            int haveCard = int.Parse(Console.ReadLine());
+            int[] Cards = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int loop = int.Parse(Console.ReadLine());
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Hashtable ht = new Hashtable();
+
+            for(int i = 0; i < haveCard; i++)
+            {
+                if (ht.ContainsKey(Cards[i]))
+                {
+                    int num = (int)ht[Cards[i]] + 1;
+                    ht.Remove(Cards[i]);
+                    ht.Add(Cards[i], num);
+                }
+                else
+                {
+                    ht.Add(Cards[i], 1);
+                }
+            }
+
+            for(int j = 0; j< loop; j++)
+            {
+                if (ht.ContainsKey(nums[j]))
+                {
+                    sb.Append(ht[nums[j]] + " ");
+                }
+                else
+                {
+                    sb.Append("0 ");
+                }
+            }
+            
+            Console.WriteLine(sb.ToString());
+            
+        }
+    }
+}
