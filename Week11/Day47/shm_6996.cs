7bbc7f65da862d306bd1ddbec4c10fc510f352ba
Seohyunmin42 <79955395+Seohyunmin42@users.noreply.github.com>
2023-03-02 16:41:13 +0900

    Day47

---
+++ Week11/Day47/shm_6996.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace _20230302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            StringBuilder sb = new StringBuilder();
+            sw.AutoFlush = true;
+            int loop = int.Parse(sr.ReadLine());
+            for(int i = 0; i < loop; i++)
+            {
+                bool chk = false;
+                string[] input = sr.ReadLine().Split();
+                char[] first = input[0].ToCharArray();
+                char[] second = input[1].ToCharArray();
+                Array.Sort(first);
+                Array.Sort(second);
+                if (first.Length != second.Length) chk = true;
+                else
+                {
+                    for (int j = 0; j < first.Length; j++)
+                    {
+                        if (first[j] != second[j])
+                        {
+                            chk = true;
+                            break;
+                        }
+                    }
+                }
+              
+                if (!chk) sb.AppendLine(input[0] + " & " + input[1] + " are anagrams.");
+                else sb.AppendLine(input[0] + " & " + input[1] + " are NOT anagrams.");
+            }
+            sw.Write(sb);
+        }
+    }
+}
